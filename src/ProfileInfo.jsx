ccf5afc2133c60cf177476981825b7d9251dccf4
Harshana <mailtoharshanaonlive@gmail.com>
2023-10-22 16:55:03 +0530

    converted to jsx

--- src/ProfileInfo.jsx
+++ src/ProfileInfo.jsx
@@ -4,95 +4,90 @@ import "./Style.css";
 const ProfileInfo = () => {
   return (
     <div>
-      <div className="gray-background">
-        <div className="profile-edit-text">Edit Your Profile</div>
-        <div className="profile-content">Profile</div>
-        <div className="profile-info">
+      <div classname="gray-background">
+        <div classname="profile-edit-text">Edit Your Profile</div>
+        <div classname="profile-content">Profile</div>
+        <div classname="profile-info">
           <p>
             This information will be displayed publicly so be carefull what you
             share
           </p>
         </div>
-
-        <div className="user-name">
+        <div classname="user-name">
           <p>Username: </p>
         </div>
-        <div className="user-text">
+        <div classname="user-text">
           <input
             type="text"
             placeholder="John Doe"
-            className="user-name-input"
+            classname="user-name-input"
           />
         </div>
-
-        <div className="user-photo">
+        <div classname="user-photo">
           <p>Photo: </p>
         </div>
-        <div className="upload">
-          <button className="upload-button">Upload</button>
+        <div classname="upload">
+          <button classname="upload-button">Upload</button>
         </div>
-        <div className="div-tag"></div>
-        <div className="profile-content">Personal information</div>
-        <div className="profile-info">
+        <div classname="div-tag" />
+        <div classname="profile-content">Personal information</div>
+        <div classname="profile-info">
           <p>Use a permanant address where you can receive mail</p>
         </div>
-
-        <div className="personal-name">
+        <div classname="personal-name">
           <div>
-            <div className="user-name">
+            <div classname="user-name">
               <p>First Name: </p>
             </div>
-            <div className="personal-text">
+            <div classname="personal-text">
               <input
                 type="text"
                 placeholder="John"
-                className="user-name-input"
+                classname="user-name-input"
               />
             </div>
           </div>
           <div>
-            <div className="user-name">
+            <div classname="user-name">
               <p>Last Name: </p>
             </div>
-            <div className="personal-text">
+            <div classname="personal-text">
               <input
                 type="text"
                 placeholder="Doe"
-                className="user-name-input"
+                classname="user-name-input"
               />
             </div>
           </div>
         </div>
-
         <div>
-          <div className="user-name">
+          <div classname="user-name">
             <p>Email address: </p>
           </div>
-          <div className="email-text">
+          <div classname="email-text">
             <input
               type="text"
               placeholder="example@gmail.com"
-              className="user-name-input"
+              classname="user-name-input"
             />
           </div>
         </div>
-
         <div>
-          <div className="user-name">
+          <div classname="user-name">
             <p>Contact Us: </p>
           </div>
-          <div className="personal-text">
+          <div classname="personal-text">
             <input
               type="text"
-              placeholder="0778564977"
-              className="user-name-input"
+              placeholder={"0778564977"}
+              classname="user-name-input"
             />
           </div>
         </div>
-        <div className="div-tag"></div>
-        <div className="btn-container">
-          <button className="cancel-button">Cancel</button>
-          <button className="save-button">Save Data</button>
+        <div classname="div-tag" />
+        <div classname="btn-container">
+          <button classname="cancel-button">Cancel</button>
+          <button classname="save-button">Save Data</button>
         </div>
       </div>
     </div>
--- src/ProfileMain.jsx
+++ src/ProfileMain.jsx
@@ -4,35 +4,34 @@ import { Link } from "react-router-dom";
 
 const ProfileMain = () => {
   return (
-    <div className="main-container">
-      <div className="heading-text">
+    <div classname="main-container">
+      <div classname="heading-text">
         <h1>Your Profile</h1>
       </div>
-      <div className="gray-background">
-        <div className="primary-container">
-          <div className="main-name">
+      <div classname="gray-background">
+        <div classname="primary-container">
+          <div classname="main-name">
             <h2>John Doe</h2>
           </div>
-          <div className="first-name">
+          <div classname="first-name">
             <h5>First Name : </h5>
-            <p className="form-data">john</p>
+            <p classname="form-data">john</p>
           </div>
-          <div className="second-name">
+          <div classname="second-name">
             <h5>Second Name : </h5>
-            <p className="form-data">Doe</p>
+            <p classname="form-data">Doe</p>
           </div>
-          <div className="email">
+          <div classname="email">
             <h5>Email : </h5>
-            <p className="form-data">johndoe@gmail.com</p>
+            <p classname="form-data">johndoe@gmail.com</p>
           </div>
-          <div className="contact-no">
+          <div classname="contact-no">
             <h5>Contact Number : </h5>
-            <p className="form-data">+94 7789456641</p>
+            <p classname="form-data">+94 7789456641</p>
           </div>
-          <div className="btn-container">
-            <Link to="./ProfileInfo">
-              <button className="save-button">Edit Profile</button>
-            </Link>
+          <div classname="btn-container">
+            <link to="./ProfileInfo" />
+            <button classname="save-button">Edit Profile</button>
           </div>
         </div>
       </div>
